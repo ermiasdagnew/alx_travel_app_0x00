b1a0c17ca2a15a20d93dda10c9944c7acade39e4
Ermias Aweke <ermiasdagnewa@gmail.com>
2025-11-14 13:09:17 +0300

    Delete listings directory

--- listings/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .views import ListingViewSet, BookingViewSet
-
-router = routers.DefaultRouter()
-router.register(r'listings', ListingViewSet)
-router.register(r'bookings', BookingViewSet)
-
-urlpatterns = router.urls
